13c9943819ab39eada1c2bfbc6dcfc7c7239a988
Yoël Pépin <blackcrown971@gmail.com>
2018-11-09 14:27:53 -0400

    Changement de cap

--- galaxy.py
+++ galaxy.py
@@ -41,86 +41,91 @@ def Pauser() :
 
 #--------------------------Corps du jeu--------------------------
 def corps():
-    rectText = imageText.get_rect()
-    rectText.x = 10
-    rectText.y = 10
+	rectText = imageText.get_rect()
+	rectText.x = 10
+	rectText.y = 10
 
-    rectText2 = imageText2.get_rect()
-    rectText2.x = 10
-    rectText2.y = 40
+	rectText2 = imageText2.get_rect()
+	rectText2.x = 10
+	rectText2.y = 40
 
-    rectText3 = imageText2.get_rect()
-    rectText3.x = 10
-    rectText3.y = 70
+	rectText3 = imageText2.get_rect()
+	rectText3.x = 10
+	rectText3.y = 70
 
-    rectFond = imageFond.get_rect()
-    rectFond.x = -320
-    rectFond.y = 0
+	rectFond = imageFond.get_rect()
+	rectFond.x = -320
+	rectFond.y = 0
 
-    rectv = imgvaisseau.get_rect()
-    rectv.x = 295
-    rectv.y = 555
+	rectv = imgvaisseau.get_rect()
+	rectv.x = 295
+	rectv.y = 555
+	tab_tir=[]
+	rectprojectil= imgprojectil.get_rect()
 
-    rectprojectil= imgprojectil.get_rect()
-    rectprojectil.x= rectv.x+(rectv.w/2)-(rectprojectil.w/2)
-    rectprojectil.y= rectv.y-20
 
-    framerate= pygame.time.Clock()
-    continuer=1
-    while continuer:
-        framerate.tick(30)
-        touched = pygame.key.get_pressed()
-        if touched [pygame.K_LEFT] and rectv.x>0:
-            rectv.x-=10
-            rectprojectil.x= rectv.x+(rectv.w/2)-(rectprojectil.w/2)
-        if touched [pygame.K_RIGHT] and rectv.x<590:
-            rectv.x+=10
-            rectprojectil.x= rectv.x+(rectv.w/2)-(rectprojectil.w/2)
-        if touched [pygame.K_UP] and rectv.y>hauteur/2:
-            rectv.y-=10
-            rectprojectil.y= rectv.y-20
-        if touched [pygame.K_DOWN] and rectv.y<575:
-            rectv.y+=10
-            rectprojectil.y= rectv.y-20
+	framerate= pygame.time.Clock()
+	continuer=1
+	while continuer:
+		framerate.tick(30)
+		touched = pygame.key.get_pressed()
+		if touched [pygame.K_LEFT] and rectv.x>0:
+			rectv.x-=10
+			rectprojectil.x= rectv.x+(rectv.w/2)-(rectprojectil.w/2)
+		if touched [pygame.K_RIGHT] and rectv.x<590:
+			rectv.x+=10
+			rectprojectil.x= rectv.x+(rectv.w/2)-(rectprojectil.w/2)
+		if touched [pygame.K_UP] and rectv.y>hauteur/2:
+			rectv.y-=10
+			rectprojectil.y= rectv.y-20
+		if touched [pygame.K_DOWN] and rectv.y<575:
+			rectv.y+=10
+			rectprojectil.y= rectv.y-20
 
-        for event in pygame.event.get() :
-            if event.type == QUIT:
-                sys.exit
-                quit()
-            if event.type == KEYUP :
-                if event.key == K_RETURN :
-                    pause=True
-                    while pause:
-                        framerate.tick()
-                        for event in pygame.event.get() :
-                            if event.type == QUIT :
-                                quit()
-                            if event.type == KEYUP :
-                                if event.key == K_RETURN :
-                                    pause = False
-                                if event.key == K_ESCAPE :
-                                    quit()
-                        Pauser2()
-                        Pauser()
-                        pygame.display.flip()
+		for event in pygame.event.get() :
+			if event.type == QUIT:
+				sys.exit
+				quit()
+			if event.type == KEYUP :
+				if event.key == K_RETURN :
+					pause=True
+					while pause:
+						framerate.tick()
+						for event in pygame.event.get() :
+							if event.type == QUIT :
+								quit()
+							if event.type == KEYUP :
+								if event.key == K_RETURN :
+									pause = False
+								if event.key == K_ESCAPE :
+									quit()
+						Pauser2()
+						Pauser()
+						pygame.display.flip()
 
-        fenetre.blit(imageFond, rectFond)
-        fenetre.blit(imgvaisseau, rectv)
-        fenetre.blit(imageText, rectText)
-        fenetre.blit(imageText2, rectText2)
-        fenetre.blit(imageText3, rectText3)
+		fenetre.blit(imageFond, rectFond)
+		fenetre.blit(imgvaisseau, rectv)
+		fenetre.blit(imageText, rectText)
+		fenetre.blit(imageText2, rectText2)
+		fenetre.blit(imageText3, rectText3)
 
-        if touched [pygame.K_SPACE]:
-            fenetre.blit(imgprojectil, rectprojectil)
-        pygame.display.flip()
+		if touched [pygame.K_SPACE]:
+			rectprojectil= imgprojectil.get_rect()
+			rectprojectil.x= rectv.x+(rectv.w/2)-(rectprojectil.w/2)
+			rectprojectil.y= rectv.y-20
+			tab_tir.append(rectprojectil)
+			fenetre.blit(imgprojectil, rectprojectil)
+		for t in tab_tir:
+			fenetre.blit(imgprojectil, t )
+		pygame.display.flip()
 
-        touched = pygame.key.get_pressed()
-        if touched [pygame.K_ESCAPE] :
-           		 	continuer=0
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                continuer=0
-        pygame.display.flip()
+		touched = pygame.key.get_pressed()
+		if touched [pygame.K_ESCAPE] :
+					continuer=0
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				continuer=0
+		pygame.display.flip()
 #----------------------------execution du programme----------------
 corps()
 pygame.quit()
